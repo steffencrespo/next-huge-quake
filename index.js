c7be9b56a9ac83e2083b35a72b2b1c2503e39f91
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-26 13:04:12 -0400

    extracts min magnitude as a parameter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const ENDPOINT_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5';
+const MIN_MAGNITUDE = '5';
+const ENDPOINT_URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=${MIN_MAGNITUDE}`;
 
 function runQuery(data) {
 	getDataFromAPI(printData);
@@ -15,8 +16,8 @@ function getDataFromAPI(callback) {
 
 function printData(data) {
 	// data.features[0].properties.place
-	$('#js-location-feed').append(`${data.features[0].properties.place} earthquakes`);
-	$('#js-quake-counter').append(`There has been ${data.features.length}`);
+	$('#js-location-feed').append(`${data.features[0].properties.place}`);
+	$('#js-quake-counter').append(`There has been ${data.features.length} earthquakes of magnitude ${MIN_MAGNITUDE}+`);
 }
 
 $(runQuery());
