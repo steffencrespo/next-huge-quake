068068b45444b9f0c2c800c9ea976a8c3449e222
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-26 13:09:19 -0400

    adds list of all places where eq happened, makes box adapt to the content

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const MIN_MAGNITUDE = '5';
+const MIN_MAGNITUDE = '4.7';
 const ENDPOINT_URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=${MIN_MAGNITUDE}`;
 
 function runQuery(data) {
@@ -16,7 +16,14 @@ function getDataFromAPI(callback) {
 
 function printData(data) {
 	// data.features[0].properties.place
-	$('#js-location-feed').append(`${data.features[0].properties.place}`);
+	const allQuakes = [];
+
+	for (let i = 0; i < data.features.length; i++) {
+		allQuakes.push(data.features[i].properties.place);
+	}
+
+	// $('#js-location-feed').append(`${data.features[0].properties.place}`);
+	$('#js-location-feed').append(`${allQuakes}`);
 	$('#js-quake-counter').append(`There has been ${data.features.length} earthquakes of magnitude ${MIN_MAGNITUDE}+`);
 }
 
