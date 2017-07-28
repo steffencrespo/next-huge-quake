33d2e4e859c3ed05f01387f0d8186fa235e990bc
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-28 13:45:16 -0400

    adds actual earthquake id as row id to enable actual details query from API later

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function runQuery(data) {
 	START_TIME = $('#js-start-date').val('01/01/2017');
 	END_TIME = $('#js-end-date').val('01/02/2017');
 	MIN_MAGNITUDE = $('#js-magnitude').val('5');
-	getDataFromAPI(printData);
+	getDataFromAPI(printHomePageData);
 	$('#js-quake-form').on('submit', function(event){
 		event.preventDefault();
-		getDataFromAPI(printData);
+		getDataFromAPI(printHomePageData);
 	});
+	handleClickOnEarthquakeRow();
 }
 
 function setDateRangeAndMagnitude() {
@@ -32,18 +33,30 @@ function getDataFromAPI(callback) {
 	$.ajax(settings);
 }
 
-function printData(data) {
+function handleClickOnEarthquakeRow() {
+	$('#js-quake-feed').on('click', 'tr', function(event) {
+		let earthquakeId = $(this).attr('id');
+		console.log(earthquakeId);
+	});
+}
+
+function printHomePageData(data) {
 	// data.features[0].properties.place
 	let allQuakes = '';
 
 	for (let i = 0; i < data.features.length; i++) {
-		allQuakes += `<tr id='js-row-${i}'><td>${data.features[i].properties.mag}</td><td>${data.features[i].properties.place}</td></tr>`
+		allQuakes += `<tr id=${data.features[i].id}><td>${data.features[i].properties.mag}</td><td>${data.features[i].properties.place}</td></tr>`
 		// allQuakes.push(data.features[i].properties.place);
 	}
 
 	// $('#js-location-feed').append(`${data.features[0].properties.place}`);
 	$('#js-quake-feed').html(`${allQuakes}`);
 	// $('#js-quake-counter').append(`There has been ${data.features.length} earthquakes of magnitude ${MIN_MAGNITUDE}+`);
+}
+
+function printEarthquakeDetails() {
+	// https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000a1jq&format=geojson
+	// https://earthquake.usgs.gov/earthquakes/eventpage/us2000a1jq#executive
 }
 
 $(runQuery());
