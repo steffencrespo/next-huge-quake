e701914bb540804620a968bd9fb4842511ee644d
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-30 13:58:40 -0400

    breaks html lines

--- index.js
+++ index.js
@@ -77,8 +77,12 @@ function _addInfoToEarthquakeDetailsPanel(earthquakeDetailedData) {
 
 	let resultsList = '<li class="list-group-item"><span class="badge">earthquakeChosenInfo[key]</span>key</li>';
 
-	Object.keys(earthquakeChosenInfo).forEach(key => $('#js-quake-panel-list').append(`<li class="list-group-item"><span class="badge">${earthquakeChosenInfo[key]}</span>${key}</li>`));
-
+	Object.keys(earthquakeChosenInfo).forEach(key => $('#js-quake-panel-list').append(`
+		<li class="list-group-item">
+			<span class="badge">${earthquakeChosenInfo[key]}</span>
+			${key}
+		</li>`
+	));
 }
 // TODO: This function is intended to call USGS details page, but right now
 // the click listener is set to listen for a click on the EQ row, not the correct behavior
